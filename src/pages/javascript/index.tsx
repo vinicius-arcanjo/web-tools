15cc622488c548f3f542db93cf6bb20128865e4b
Vinicius Arcanjo <vinicius@arcanjo.dev>
2022-07-14 01:58:09 -0300

    Added javascript playground

---
+++ src/pages/javascript/index.tsx
@@ -0,0 +1,10 @@
+import Javascript from 'templates/Javascript'
+
+export default function Index() {
+  return (
+    <Javascript
+      title="Javascript Playground"
+      code="console.log('Hello World!')"
+    />
+  )
+}
