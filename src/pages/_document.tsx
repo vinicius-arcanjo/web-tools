c3f0e6bfeae7a3c2095c62ffea4c4234ad5a0698
Vinicius Arcanjo <vinicius@arcanjo.dev>
2022-07-19 01:08:24 -0300

    Added Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Analytics from '../components/Analytics'
 
 export default class MyDocument extends Document {
   render() {
@@ -8,6 +9,7 @@ export default class MyDocument extends Document {
         <body className="bg-white text-slate-500 antialiased transition-colors duration-500 dark:bg-slate-900 dark:text-slate-400">
           <Main />
           <NextScript />
+          <Analytics />
         </body>
       </Html>
     )
